d0cdd89b59860516f682591ec79e224a97f919eb
Ad-Bean <adbeanx@outlook.com>
2023-03-21 21:08:26 +0800

    feat: mask imdb done

--- functions/bert-mlm-imdb/bert-mlm-imdb/handler.py
+++ functions/bert-mlm-imdb/bert-mlm-imdb/handler.py
@@ -1,6 +1,8 @@
 import json
 import torch
-from transformers import pipeline, AutoTokenizer, AutoModel
+from transformers import pipeline, logging, AutoTokenizer, AutoModel
+
+logging.set_verbosity_error()
 
 model_name = "huggingface-course/distilbert-base-uncased-finetuned-imdb"
 model_state_dict = torch.load('bert_model.pth')
@@ -12,11 +14,9 @@ model = AutoModel.from_pretrained(model_name, state_dict=model_state_dict)
 def handle(req):
     body = json.loads(req)
     text = body["text"]
-    model = body["model_name"]
+    model_name = body["model_name"]
 
-    mask_filler = pipeline(
-        "fill-mask", model=model
-    )
+    mask_filler = pipeline("fill-mask", model=model_name)
     ans = mask_filler(text, top_k=1)
 
     res = {
@@ -25,4 +25,5 @@ def handle(req):
         "token_str": ans[0]["token_str"],
         "sequence": ans[0]["sequence"]
     }
+
     return json.dumps(res)
